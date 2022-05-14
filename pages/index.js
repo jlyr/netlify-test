fca1a8123a7177d4cac1947fd254bff5ba4370b8
jlyr <80109128+jlyr@users.noreply.github.com>
2022-05-15 00:45:19 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Hola" />
+        <Header title="pfmsd" />
         <p className="description">
           "eh cb la" <code>pages/index.js</code>
         </p>
